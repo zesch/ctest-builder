3ca35fe0ba9a739e5e4aa0d9e653cd70aa7ab026
coocoorhin <ziyaowang@Ziyaos-iMac.local>
2017-10-24 22:42:53 +0200

    #23
    Function added in api

--- demo/src/main/java/com/nlpuni/demo/MyResource.java
+++ demo/src/main/java/com/nlpuni/demo/MyResource.java
@@ -2,10 +2,12 @@ package com.nlpuni.demo;
 
 import java.util.List;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
 import org.json.simple.JSONObject;
@@ -25,10 +27,11 @@ public class MyResource {
 	
 	private TokenizerService tokenizerService = new TokenizerService();
 
-    @POST
+    @POST 
     @Produces(MediaType.APPLICATION_JSON)
-    public List<TokenDTO> getIt(String text) {
+    public List<TokenDTO> getIt(String text, @QueryParam("LanID") String LanID ) {
     		System.out.println(text);
+    		System.out.println(LanID);
     		return tokenizerService.getTokens(text);
     }
     
