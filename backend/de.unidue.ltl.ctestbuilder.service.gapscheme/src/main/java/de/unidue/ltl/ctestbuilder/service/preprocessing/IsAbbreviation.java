a80620c7ae4c66d58423ae6268b20f32083e8a14
Marius Hamacher <marius.hamacher@outlook.de>
2018-07-12 09:14:38 +0200

    Add abbreviations.

--- backend/de.unidue.ltl.ctestbuilder.service.gapscheme/src/main/java/de/unidue/ltl/ctestbuilder/service/preprocessing/IsAbbreviation.java
+++ backend/de.unidue.ltl.ctestbuilder.service.gapscheme/src/main/java/de/unidue/ltl/ctestbuilder/service/preprocessing/IsAbbreviation.java
@@ -1,5 +1,9 @@
 package de.unidue.ltl.ctestbuilder.service.preprocessing;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -8,52 +12,48 @@ import java.util.function.Predicate;
 import java.util.regex.Pattern;
 
 import de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Token;
+import de.unidue.ltl.ctestbuilder.service.gapscheme.GapScheme;
 
 public class IsAbbreviation implements Predicate<Token> {
 	
-	private static Map<String, String[]> LANGUAGE_ABBREVIATIONS;
+	private static Pattern ABBREVIATION_PATTERN = Pattern.compile("([a-zA-z]+\\.[-]?)+");
+	private static Map<String, Pattern[]> LANGUAGE_ABBREVIATIONS;	
+	
+	private Pattern[] abbreviations;
+	
 	static {
-		//TODO: fill with abbreviations
-		String[] german = new String[] {
-				"bzgl\\.",
-				"usw\\.",
-				"u\\.A\\."
-		};
-		String[] english = new String[] {};
-		String[] french = new String[] {};
-		String[] spanish = new String[] {};
-		String[] italian = new String[] {};
-		String[] finnish = new String[] {};
-		
 		LANGUAGE_ABBREVIATIONS = new HashMap<>();
-		LANGUAGE_ABBREVIATIONS.put("de", german);
-		LANGUAGE_ABBREVIATIONS.put("en", english);
-		LANGUAGE_ABBREVIATIONS.put("fr", french);
-		LANGUAGE_ABBREVIATIONS.put("es", spanish);
-		LANGUAGE_ABBREVIATIONS.put("it", italian);
-		LANGUAGE_ABBREVIATIONS.put("fi", finnish);
+		
+		for (String language : GapScheme.SUPPORTED_LANGUAGES) {
+			Path file = Paths.get(String.format("src/main/resources/abbreviations_%s.txt", language));			
+			try {
+				Pattern[] abbreviations = Files.lines(file).map(Pattern::compile).toArray(Pattern[]::new);
+				LANGUAGE_ABBREVIATIONS.put(language, abbreviations);
+			} catch (IOException e) {
+				LANGUAGE_ABBREVIATIONS.put(language, new Pattern[0]);
+				System.err.println("WARNING: Could not read file@" + file.toString());
+				System.err.println("\tCheck if file exists and is readable.");
+			}
+		}
 	}
 	
-	private Pattern abbreviationPattern = Pattern.compile("([a-zA-z]+\\.)+");
-	private List<Pattern> abbreviations;
-	
 	public IsAbbreviation(String language) {
-		String[] languageAbbreviations = LANGUAGE_ABBREVIATIONS.get(language);
-		if (languageAbbreviations == null)
-			languageAbbreviations = new String[] {};
-		
-		abbreviations = new ArrayList<>();
-		for (String abbreviation : languageAbbreviations)
-			abbreviations.add(Pattern.compile(abbreviation));		
+		if (LANGUAGE_ABBREVIATIONS.containsKey(language))
+			abbreviations = LANGUAGE_ABBREVIATIONS.get(language);
+		else
+			abbreviations = new Pattern[0];
 	}
 	
 	@Override
 	public boolean test(Token token) {
+		if (ABBREVIATION_PATTERN.matcher(token.getCoveredText()).matches())
+			return true;
+		
 		for (Pattern abbreviation : abbreviations) {
 			if (abbreviation.matcher(token.getCoveredText()).matches())
 				return true;
 		}
 		
-		return abbreviationPattern.matcher(token.getCoveredText()).matches();
+		return false;
 	}
 }
