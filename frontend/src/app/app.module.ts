7164c3433fed5a5a70baaaed62751b1c541a1467
marvinwest <marvin.westphal@stud.uni-due.de>
2022-03-10 15:40:49 +0100

    Adds initially compiling home-component (not yet visible)

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,21 +4,32 @@ import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MatCardModule } from '@angular/material/card';
 
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatTabsModule } from '@angular/material/tabs';
 import { StateManagementService } from './services/state-management.service';
 import { CtestService } from './services/ctest.service';
+import { HomeComponent } from './components/home/home.component';
 import { HttpClientModule } from '../../node_modules/@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
+    FormsModule,
+    ReactiveFormsModule,
     BrowserModule,
     FlexLayoutModule,
     AppRoutingModule,
     MatCardModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatTabsModule,
     HttpClientModule
   ],
   providers: [
---
+++ frontend/src/app/components/home/home.component.ts
@@ -0,0 +1,150 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { CtestService } from '../../services/ctest.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { switchMap } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+  /**
+   * The form containing the text to send to the service.
+   */
+  public form: FormGroup;
+
+  /** holds value indicate if user uploaded wrong files or not */
+  public invalidType = false;
+
+  /** holds available system languages */
+  public languages = [
+    { value: 'en', viewValue: 'English' },
+    { value: 'fr', viewValue: 'French' },
+    { value: 'de', viewValue: 'German' },
+    { value: 'fi', viewValue: 'Finnish' },
+    { value: 'es', viewValue: 'Spanish' },
+    { value: 'it', viewValue: 'Italian' }
+  ];
+
+  /**
+   * Indicates whether the c-test service is available.
+   */
+  public serviceAvailable: boolean;
+
+  /**
+   * The title of the c-test.
+   */
+  public title: string;
+
+  // Life Cycle Hooks
+  constructor(
+    private _fb: FormBuilder,
+    private ctestService: CtestService,
+    private router: Router,
+    private snackBar: MatSnackBar
+  ) { }
+
+  ngOnInit(): void {
+    this.form = this._fb.group({
+      ctestTitle: ['', Validators.required],
+      ctestText: ['', Validators.required],
+    });
+
+    this.ctestService.verify().subscribe(
+      success => {
+        console.log(success);
+        this.serviceAvailable = true;
+      },
+      failure => {
+        console.error('GapScheme Service is not available.');
+        this.serviceAvailable = false;
+        this.snackBar.open('GapScheme Service is not available.', 'OK');
+      }
+    );
+  }
+
+  /**
+   * when user selects a txt file
+   * "!" means we expect a file to be there and not be null (otherwise a guard is needed,
+   * see: https://stackoverflow.com/questions/49431880/ts2531-object-is-possibly-null)
+   * @param  {event} Event holds value of event that holds HTMLInputElement
+   */
+  public load($event: any) {
+    const input = $event.target as HTMLInputElement;
+    // Validate the input is in .txt format
+    if (/(\.txt|\.TXT)$/.test(input.value)) {
+      const reader = new FileReader();
+      reader.onload = () => {
+        this.title = this.fileNameToTitle(input.files![0].name.split('.')[0]);
+        this.form.controls['ctestText'].setValue(reader.result);
+      };
+      reader.readAsText(input.files![0]);
+      this.invalidType = false;
+    } else {
+      this.invalidType = true;
+      this.form.controls['ctestText'].reset('');
+    }
+  }
+
+  /**
+   * Imports a c-test from file.
+   */
+  public import($event: any) {
+    const input = $event.target as HTMLInputElement;
+    const reader = new FileReader();
+    reader.onloadstart = () => {
+      this.snackBar.open('Importing C-Test...', 'OK');
+    };
+    reader.onload = () => {
+      const fileName = input.files![0].name;
+      this.title = this.fileNameToTitle(fileName.split('.')[0]);
+      this.ctestService.setTitle(this.title);
+      const text = reader.result as string;
+      console.log(text);
+      if (fileName.endsWith('.json')) {
+        const ctest = JSON.parse(text);
+        this.ctestService.setCTest(ctest);
+        this.snackBar.open('Done!', 'OK', {duration: 1250});
+        this.router.navigate(['/text-editor']);
+      } else { // endsWith(.xml)
+        this.ctestService.fetchCTestFromJACK(text).subscribe(
+          success => {
+            this.snackBar.open('Done!', 'OK', {duration: 1250});
+            this.router.navigate(['/text-editor']);
+          },
+          failure => this.snackBar.open('ERROR: Could not reimport C-Test.', 'OK'));
+      }
+    };
+    reader.readAsText(input.files![0]);
+  }
+
+  private fileNameToTitle(fileName: string) {
+    return fileName.replace(/_/g, ' ');
+  }
+
+  /**
+   * Submits the form, requests the C-Test and navigates to the text-edit view.
+   */
+  public submit() {
+    if (this.form.valid && this.serviceAvailable) {
+      this.snackBar.open('Processing text, please be patient...', 'OK');
+
+      this.ctestService.setTitle(this.title);
+      const text = this.form.controls['ctestText'].value;
+      this.ctestService.identifyLanguage(text).pipe(
+        switchMap(language => this.ctestService.fetchCTest(text, language))
+      ).subscribe(
+        success => {
+          this.snackBar.open('Done!', 'OK', {duration: 1250});
+          this.router.navigate(['/text-editor']);
+        },
+        failure => {
+          this.snackBar.open('ERROR: Could not process text!', 'OK', {duration: 2500});
+        }
+      );
+    }
+  }
+}
