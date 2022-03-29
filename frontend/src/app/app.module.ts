b695d862f9c7f060659a720cb2fcc51468f2b4f6
marvinwest <marvin.westphal@stud.uni-due.de>
2022-03-29 14:13:47 +0200

    fix up project to let it compile initially

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -12,12 +12,38 @@ import { HomeComponent } from './components/home/home.component';
 import { HttpClientModule } from '../../node_modules/@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ModalDialogComponent } from './components/modal-dialog/modal-dialog.component';
+import { TextEditComponent } from './components/text-edit/text-edit.component';
+import { TokenComponent } from './components/text-edit/token/token.component';
+import { HelpPageComponent } from './components/text-edit/help-page/help-page.component';
+import { ExportComponent } from './components/text-edit/export/export.component';
+import { ColoredGapsPipe } from './pipes/colored-gaps.pipe';
+import { IosviewPipe } from './pipes/iosview.pipe';
+import { JackViewPipe } from './pipes/jack-view.pipe';
+import { PlainSolutionsPipe } from './pipes/plain-solutions.pipe';
+import { PromptPipe } from './pipes/prompt.pipe';
+import { ReplacePipe } from './pipes/replace.pipe';
+import { SolutionPipe } from './pipes/solution.pipe';
+import { SolutionsPipe } from './pipes/solutions.pipe';
+import { TestviewPipe } from './pipes/testview.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    ModalDialogComponent
+    ModalDialogComponent,
+    TextEditComponent,
+    TokenComponent,
+    HelpPageComponent,
+    ExportComponent,
+    ColoredGapsPipe,
+    IosviewPipe,
+    JackViewPipe,
+    PlainSolutionsPipe,
+    PromptPipe,
+    ReplacePipe,
+    SolutionPipe,
+    SolutionsPipe,
+    TestviewPipe
   ],
   imports: [
     FormsModule,
@@ -31,7 +57,7 @@ import { ModalDialogComponent } from './components/modal-dialog/modal-dialog.com
   providers: [
     CtestService,
     StateManagementService],
-  entryComponents: [ModalDialogComponent],
+  entryComponents: [ModalDialogComponent, HelpPageComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
