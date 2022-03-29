d4ea83230de8de7096e86851c43a19b282df7726
marvinwest <marvin.westphal@stud.uni-due.de>
2022-03-29 11:56:50 +0200

    adds jack-view pipe

---
+++ frontend/src/app/pipes/jack-view.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { JackViewPipe } from './jack-view.pipe';
+
+describe('JackViewPipe', () => {
+  it('create an instance', () => {
+    const pipe = new JackViewPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
