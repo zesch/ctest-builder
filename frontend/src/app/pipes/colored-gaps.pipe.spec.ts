f2e9555f281b18305debfc8567dd6cbd593ef121
marvinwest <marvin.westphal@stud.uni-due.de>
2022-03-29 11:52:53 +0200

    adds colored-gaps pipe

---
+++ frontend/src/app/pipes/colored-gaps.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { ColoredGapsPipe } from './colored-gaps.pipe';
+
+describe('ColoredGapsPipe', () => {
+  it('create an instance', () => {
+    const pipe = new ColoredGapsPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
